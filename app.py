088ceda2435171f4cc0baf59af525107609fc192
Klem C <2006919@aluno.univesp.br>
2021-11-05 15:18:47 -0300

    Endereçamento das variáveis para criação dos mapas e endereços!

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///descartes.sqlite3'
@@ -11,13 +12,13 @@ db = SQLAlchemy(app)
 Bootstrap()
 
 
-# Tabala Produto
+# Tabala Produtos
 class Produtos(db.Model):
     id = db.Column('produto_id', db.Integer, primary_key=True)
     item = db.Column(db.String(100))
 
 
-# Tabela Destino
+# Tabela Destinos
 class Destinos(db.Model):
     id = db.Column('destino_id', db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
@@ -26,6 +27,7 @@ class Destinos(db.Model):
     mapa = db.Column(db.String(10))
 
 
+# Tabela Descartes
 class Descartes(db.Model):
     id = db.Column('descarte_id', db.Integer, primary_key=True)
     produto_id = db.Column(db.Integer, ForeignKey(Produtos.id))
@@ -42,26 +44,32 @@ def __init__(self, item, nome, endereco, telefone, mapa, produto_id, destino_id)
     self.destino_id = destino_id
 
 
+# Carrega página principal
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global busca
     if request.method == 'POST':
         if not request.form['item']:
             flash('Por Favor selecione um dos items', 'error')
         else:
             busca = request.form['item']
+           # dados = Destinos.query.filter(Descartes.produto_id == busca).all()
 
-            print(busca)
+            #for linha in dados:
+              #  print(f'Nome: {linha.produto_id} ')
+
             flash('Record was successfully added')
-            return redirect(url_for('destino'))
+            return redirect(url_for('destinos'))
     return render_template("index.html")
 
 
-@app.route('/destino')
-def destino():
-    return render_template('destinos.html', descartes=Descartes.query.all(),
-                           produtos=Produtos.query.all(), destinos=Destinos.query.all())
+# Carrega página que mostra os pontos de descarte na cidade
+@app.route('/destinos')
+def destinos():
+    return render_template('destinos.html', local=Destinos.query.filter(Destinos.id == busca).all())
 
 
+# Carrega a página com os feedbacks
 @app.route('/opiniao')
 def opiniao():
     return render_template("opiniao.html")
