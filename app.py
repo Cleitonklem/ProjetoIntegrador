02b06f24814c223da5ac119ef74ac0414d707cae
Klem C <2006919@aluno.univesp.br>
2021-11-05 15:18:47 -0300

    Endereçamento das variáveis para criação dos mapas e endereços!

--- app.py
+++ app.py
@@ -1,72 +1,88 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-from sqlalchemy import ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import sessionmaker, relationship
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///descartes.sqlite3'
-app.config['SECRET_KEY'] = "random string"
-
-db = SQLAlchemy(app)
+engine = create_engine('sqlite:///descartes.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
 Bootstrap()
+app = Flask(__name__)
 
 
-# Tabala Produto
-class Produtos(db.Model):
-    id = db.Column('produto_id', db.Integer, primary_key=True)
-    item = db.Column(db.String(100))
+# Tabela Descartes
+class Descarte(Base):
+    __tablename__ = 'descartes'
+    id = Column(Integer, primary_key=True)
+    produto_id = Column(Integer, ForeignKey('produtos.id'))
+    destino_id = Column(Integer, ForeignKey('destinos.id'))
+    produto = relationship('Produto')
+    destino = relationship('Destino')
+
+    def __repr__(self):
+        return f'Descarte(item={self.produto.item}, nome={self.destino.nome}, endereco={self.destino.endereco}, ' \
+               f' telefone={self.destino.telefone}, mapa={self.destino.mapa})'
 
 
-# Tabela Destino
-class Destinos(db.Model):
-    id = db.Column('destino_id', db.Integer, primary_key=True)
-    nome = db.Column(db.String(100))
-    endereco = db.Column(db.String(50))
-    telefone = db.Column(db.String(200))
-    mapa = db.Column(db.String(10))
+# Tabela Destinos
+class Destino(Base):
+    __tablename__ = 'destinos'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(100))
+    endereco = Column(String(50))
+    telefone = Column(String(200))
+    mapa = Column(String(10))
+    produto_id = Column(Integer, ForeignKey('produtos.id'))
+    desc_dest = relationship(Descarte, backref='destinos')
+
+    def __repr__(self):
+        return f'Destino({self.nome}, {self.endereco}, {self.telefone}, {self.mapa})'
 
 
-class Descartes(db.Model):
-    id = db.Column('descarte_id', db.Integer, primary_key=True)
-    produto_id = db.Column(db.Integer, ForeignKey(Produtos.id))
-    destino_id = db.Column(db.Integer, ForeignKey(Destinos.id))
+# Tabala Produtos
+class Produto(Base):
+    __tablename__ = 'produtos'
+    id = Column(Integer, primary_key=True)
+    item = Column(String(100))
+    desc_pro = relationship(Descarte, backref='produtos')
+    dest_pro = relationship(Destino, backref='produtos')
+
+    def __repr__(self):
+        return f'Produto({self.item})'
 
 
-def __init__(self, item, nome, endereco, telefone, mapa, produto_id, destino_id):
-    self.item = item
-    self.nome = nome
-    self.endereco = endereco
-    self.telefone = telefone
-    self.mapa = mapa
-    self.produto_id = produto_id
-    self.destino_id = destino_id
+global endereco
 
 
-@app.route('/', methods=['GET', 'POST'])
+# Carrega página principal
+@app.route('/', methods=['GET', 'POST'],)
 def index():
+    global endereco
     if request.method == 'POST':
         if not request.form['item']:
-            flash('Por Favor selecione um dos items', 'error')
+            return render_template("index.html")
+            #flash('Por Favor selecione um dos items', 'error')
         else:
-            busca = request.form['item']
-
-            print(busca)
-            flash('Record was successfully added')
-            return redirect(url_for('destino'))
+            endereco = session.query(Destino).filter(Destino.produto_id == request.form['item']).all()
+            print(endereco)
+            return render_template('destinos.html', endereco=endereco)
+            #return redirect(url_for('destinos'))
     return render_template("index.html")
 
 
-@app.route('/destino')
-def destino():
-    return render_template('destinos.html', descartes=Descartes.query.all(),
-                           produtos=Produtos.query.all(), destinos=Destinos.query.all())
+# Carrega página que mostra os pontos de descarte na cidade
+@app.route('/destinos')
+def destinos():
+    return render_template('destinos.html', endereco=endereco)
 
 
+# Carrega a página com os feedbacks
 @app.route('/opiniao')
 def opiniao():
     return render_template("opiniao.html")
 
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
