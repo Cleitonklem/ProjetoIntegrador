6260f8d5c2dcc761fdb56bd48a7556e5643a0cc2
Klem C <2006919@aluno.univesp.br>
2021-11-10 00:57:35 -0300

    atualização

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
@@ -31,9 +31,9 @@ class Destino(Base):
     __tablename__ = 'destinos'
     id = Column(Integer, primary_key=True)
     nome = Column(String(100))
-    endereco = Column(String(50))
-    telefone = Column(String(200))
-    mapa = Column(String(10))
+    endereco = Column(String(100))
+    telefone = Column(String(15))
+    mapa = Column(String)
     desc_dest = relationship(Descarte, backref='destinos')
 
     def __repr__(self):
@@ -51,6 +51,19 @@ class Produto(Base):
         return f'Produto({self.item})'
 
 
+# Tabela para Cadastro
+class Cadastro(Base):
+    __tablename__ = 'cadastros'
+    id = Column(Integer, primary_key=True)
+    cad_item = Column(String(100))
+    cad_nome = Column(String(100))
+    cad_endereco = Column(String(100))
+    cad_telefone = Column(String(15))
+
+    def __repr__(self):
+        return f'Cadastro({self.cad_item},{self.cad_nome}, {self.cad_endereco}, {self.cad_telefone})'
+
+
 global endereco
 
 
@@ -60,7 +73,6 @@ def index():
     global endereco
     if request.method == 'POST':
         if not request.form['item']:
-            flash('Please enter all the fields', 'error')
             return render_template("index.html")
         else:
             endereco = session.query(Descarte).filter(Descarte.produto_id == request.form['item']).all()
@@ -82,9 +94,27 @@ def opiniao():
 
 
 # Carrega a página para cadastrar itens e locais novos
-@app.route('/cadastrar')
+@app.route('/cadastrar', methods=['GET', 'POST'])
 def cadastrar():
+    if request.method == 'POST':
+        cad_item = request.form['cad_item']
+        cad_nome = request.form['cad_nome']
+        cad_endereco = request.form['cad_endereco']
+        cad_telefone = request.form['cad_telefone']
+
+        cad_dados = Cadastro(cad_item, cad_nome, cad_endereco, cad_telefone)
+       # session.add(cad_dados)
+        #session.commit()
+
+       # return render_template("index.html")
+
     return render_template("cadastrar.html")
+
+
+@app.route('/espera')
+def espera():
+    esperando = Cadastro.query.all()
+    return render_template("espera.html", esperando=esperando)
 
 
 if __name__ == '__main__':
